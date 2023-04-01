139faab833a60d27cb4464bed11274e694e0e7ea
kpm hana <kpmhana@gmail.com>
2023-04-01 21:57:08 +0530

    FInal

--- 2sum.py
+++ 2sum.py
@@ -1,4 +1,20 @@
-#2 Sum
-print("2 sum")
-print("from VScode")
-print("hello")
+
+class Solution:
+    def twoSum(nums, target):
+        d = {}
+        for i in range(0, len(nums)):
+            c = target - nums[i]
+            
+            # If c exists in d, then there is a compliment - at places d[c] and i
+            if c in d.keys():   #didn;t put brackets
+                # we found it
+                print(i, d[c])
+                return
+            else:
+                d[nums[i]] = i
+
+            
+        print("Not found")
+
+
+Solution.twoSum([3,1,2,8,2],9)

5470cb57c204a924355f4421500bad40930ba4df
kpm hana <kpmhana@gmail.com>
2023-04-04 21:36:10 +0530

    Daily

---
+++ 121_best_time_to_buy_sell_stock.py
@@ -0,0 +1,31 @@
+
+class Solution:
+    def maxProfit(self, prices) -> int:
+        print(prices)
+
+        profit = 0
+        i = 0
+        min = prices[i]
+        max = prices[i]
+
+        while(i+1 < len(prices)):
+
+            if(prices[i] < min):
+                min = prices[i]
+                max = prices[i]
+
+            if(prices[i+1] > max):
+                max = prices[i+1]
+                profit = max - min
+                
+            i = i + 1
+
+        return profit
+
+
+
+#p = [7,1,5,3,6,4,19,9]
+#p = [1,7,6,4,3,18]
+p = [3,2,6,5,0,3]
+s1 = Solution
+print(s1.maxProfit(s1,p))
